ac7f6cc3a504f92d0532547566d43a2d8ae82943
Vaishnavi Adepu <postboxva6@gmail.com>
2025-06-20 13:38:49 -0400

    schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "sonner";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -70,6 +71,7 @@ export default function RootLayout({
           enableSystem
           storageKey="notion-theme"
         >
+          <Toaster position="bottom-center"/>
           {children}
         </ThemeProvider>
         </ConvexClientProvider>
